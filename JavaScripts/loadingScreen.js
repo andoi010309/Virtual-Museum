b3a96d06c27439b00aa2bf985216cf0b57108dc9
TheUserSim <simueldel19@gmail.com>
2025-03-27 19:34:10 +0800

    Update loadingScreen.js

--- JavaScripts/loadingScreen.js
+++ JavaScripts/loadingScreen.js
@@ -1,7 +1,7 @@
 const loadingScreen = document.getElementById("loadingScreen");
 
-window.onload = setTimeout(function() {
+window.onload = function() {
     loadingScreen.style.opacity = "0%";
     loadingScreen.style.pointerEvents = "none";
     loadingScreen.style.transition = "1s";
-}, 2000)
+};
